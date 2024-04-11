78e699f585cfd096d0be951329d9f3d38a25457b
Ay-dotcode <joseyowolabi@gmail.com>
2024-04-11 10:36:56 +0300

    bloc setup

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -1,13 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+import 'package:weather_app/data/my_data.dart';
 
 part 'weather_bloc_event.dart';
 part 'weather_bloc_state.dart';
 
 class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
   WeatherBlocBloc() : super(WeatherBlocInitial()) {
-    on<WeatherBlocEvent>((event, emit) {
-      //
+    on<FetchWeather>((event, emit) async {
+      emit(WeatherBlocLoading());
+      try {
+        WeatherFactory wf = WeatherFactory(apiKey, language: Language.ENGLISH);
+        Position position = Geolocator.getCurrentPosition() as Position;
+        Weather weather = await wf.currentWeatherByLocation(
+          position.latitude,
+          position.longitude,
+        );
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
     });
   }
 }
---
+++ lib/data/my_data.dart
@@ -0,0 +1,1 @@
+String apiKey = "24b064b65eb51a50b0d61b9550f093f1";
