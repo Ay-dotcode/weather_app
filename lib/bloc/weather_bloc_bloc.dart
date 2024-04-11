93c74b7ffb8c0ac5c825cb344755cc5cd95acb9a
Ay-dotcode <joseyowolabi@gmail.com>
2024-04-11 11:11:33 +0300

    setup get weather

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -13,11 +13,12 @@ class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
       emit(WeatherBlocLoading());
       try {
         WeatherFactory wf = WeatherFactory(apiKey, language: Language.ENGLISH);
-        Position position = Geolocator.getCurrentPosition() as Position;
+
         Weather weather = await wf.currentWeatherByLocation(
-          position.latitude,
-          position.longitude,
+          event.position.latitude,
+          event.position.longitude,
         );
+        print(weather);
         emit(WeatherBlocSuccess(weather));
       } catch (e) {
         emit(WeatherBlocFailure());
